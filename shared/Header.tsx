5e6e11dca3d0da205f3cfbee099662b1bbff6449
MithilaKhan <mithilakhan082@gmail.com>
2025-11-19 23:25:54 +0600

    update header

--- shared/Header.tsx
+++ shared/Header.tsx
@@ -1,59 +1,46 @@
 
 import React from 'react'
-
 import { FaRegBell } from 'react-icons/fa6';
 import { Badge } from 'antd';
 import Link from 'next/link';
-
-
+import { HiMenu } from 'react-icons/hi';
 
 const Header = () => {
 
-    // useEffect(() => {
-
-    //     const socket = io(socketURL, {
-    //       query: { token: localStorage.getItem("creatorToken") }, 
-    //     });
-
-
-    //     socket.on('get-notification::677cc986a7e6ba1aff44bd42', () => {
-
-    //       setNotificationCount((prevCount) => prevCount + 1);
-    //     });
-
-
-    //     return () => {
-    //       socket.disconnect();
-    //     };
-    //   }, []);  
-
-
     return (
-        <div className='flex items-center gap-x-7 justify-end'>
-            <Link href="/notification" className='h-fit pt-2'>
-                <Badge count={1} >
-                    <FaRegBell color="#7a777a" size={24} />
-                </Badge>
-            </Link>
+        <div className=' w-full flex items-center md:justify-end justify-between md:ps-7 ps-0'>
+            <button
+                className="lg:hidden block text-[#ABABAB] text-2xl mr-auto"
+                // onClick={toggle}
+            >
+                <HiMenu size={28} />
+            </button>
+            <div className='flex items-center gap-x-7 justify-end'>
+                <Link href="/notification" className='h-fit pt-2'>
+                    <Badge count={1} >
+                        <FaRegBell color="#7a777a" size={24} />
+                    </Badge>
+                </Link>
 
-            <Link href="/profile" className='flex  items-center gap-x-3'>
-                <img
-                    style={{
-                        clipPath: "circle()",
-                        width: 48,
-                        height: 48,
-                    }}
-                    // src={profile?.data?.profile?.startsWith("https") ? profile?.data?.profile : `${imageUrl}${profile?.data?.profile}`} 
-                    src="https://i.pinimg.com/736x/7b/05/51/7b0551406cd7936252123558aacc9191.jpg"
-                    alt="person-male--v2"
-                    className='clip'
-                />
-                <div className=' flex flex-col gap-y-0.5'>
-                    <p className=' text-[16px] font-medium'> John Smith</p>
-                    <p className=' text-[14px] font-medium'>250-12-0041</p>
-                </div>
+                <Link href="/profile" className='flex  items-center gap-x-3'>
+                    <img
+                        style={{
+                            clipPath: "circle()",
+                            width: 48,
+                            height: 48,
+                        }}
+                        // src={profile?.data?.profile?.startsWith("https") ? profile?.data?.profile : `${imageUrl}${profile?.data?.profile}`} 
+                        src="https://i.pinimg.com/736x/7b/05/51/7b0551406cd7936252123558aacc9191.jpg"
+                        alt="person-male--v2"
+                        className='clip'
+                    />
+                    <div className=' flex flex-col gap-y-0.5'>
+                        <p className=' text-[16px] font-medium'> John Smith</p>
+                        <p className=' text-[14px] font-medium'>250-12-0041</p>
+                    </div>
 
-            </Link>
+                </Link>
+            </div>
         </div>
     )
 }
